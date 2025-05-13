bfa4e561bb714ef75cd80d498ef4956a0a4ad498
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-05-14 05:28:44 +1000

    Protect from infinite loops (max 100 pages) and add TAS to address if missing

--- scraper.rb
+++ scraper.rb
@@ -23,34 +23,35 @@ def parse_application(accordion_item)
     address = description.split(" - ").last.strip rescue ""
   end
 
-  # Get more info from the accordion body if available
   more_info = accordion_item.at(".accordion-body")
   info_url = nil
   if more_info && more_info.at(".plan-file-list__item")
     info_url = more_info.at(".plan-file-list__item")["href"]
   end
 
-  record = {
+  address = "#{address} TAS" unless address.include?("TAS")
+
+  # Uncomment this when we have more information about dates
+  # "date_received" => date_received ? Date.parse(date_received).to_s : nil,
+  # "on_notice_to" => on_notice_to ? Date.parse(on_notice_to).to_s : nil,
+
+  {
     "council_reference" => council_reference,
     "description" => description_text,
     "address" => address,
     "date_scraped" => Date.today.to_s,
     "info_url" => info_url
   }
-
-  # Uncomment this when we have more information about dates
-  # "date_received" => date_received ? Date.parse(date_received).to_s : nil,
-  # "on_notice_to" => on_notice_to ? Date.parse(on_notice_to).to_s : nil,
-
-  return record
 end
 
-def scrape_page(url)
+def scrape_page(url, max_pages)
+  max_pages -= 1
+  return if max_pages < 0
+
   agent = Mechanize.new
   agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
   page = agent.get(url)
   
-  # Find all accordion grid items
   page.search(".accordion-grid-item").each do |item|
     record = parse_application(item)
     next if record.nil?
@@ -59,15 +60,12 @@ def scrape_page(url)
     ScraperWiki.save_sqlite(["council_reference"], record)
   end
   
-  # Check for next page link
   next_link = page.at(".pagination .next")
   if next_link && !next_link.attr("class").include?("disabled")
     next_url = next_link["href"]
     puts "Following next page: #{next_url}"
-    scrape_page(next_url)
+    scrape_page(next_url, max_pages)
   end
 end
 
-# Start scraping from the first page
-scrape_page("https://www.huonvalley.tas.gov.au/development/planning/advertised-applications/")
-
+scrape_page("https://www.huonvalley.tas.gov.au/development/planning/advertised-applications/", 100)
